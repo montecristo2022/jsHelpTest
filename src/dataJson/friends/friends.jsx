a30a8e3e622d2c3ad94524368dac235a3c1a0273
montecristo2022 <104501641+montecristo2022@users.noreply.github.com>
2022-09-17 17:47:16 +0300

    finish

--- src/dataJson/friends/friends.jsx
+++ src/dataJson/friends/friends.jsx
@@ -1,9 +1,27 @@
 import styles from '../friends/Friends.module.css';
 // import friends from '../dataJson/friends.json';
 import PropTypes from "prop-types";
+import peoples from '../friends.json';
 
 
-export const FriendList = ({ friends }) => {
+
+// for (const el of peoples) {
+//   if (el.isOnline === false) {
+//     isOnline = styles.statisticStatusRed
+//   }
+//   if (el.isOnline === true) {
+//     isOnline = styles.statisticStatusGreen
+//   }
+ 
+// }
+
+
+
+
+
+
+
+export const FriendList = ({  friends }) => {
 
 
 
@@ -13,17 +31,38 @@ export const FriendList = ({ friends }) => {
     <ul className={styles.friendsContainer}>
    
                 {friends.map(friend => (
-         
+       
           <li key={friend.id}className={styles.eachFriend}>
-                <span className={styles.statisticStatusGreen}></span>
+                <span className={friend.isOnline ? styles.statisticStatusGreen : styles.statisticStatusRed}></span>
             <span>{friend.name}</span>
             <img className={styles.friendsImg} src={friend.avatar}></img>
           </li>
         ))}
       
     </ul>
+      
+      
+    // <ul className={styles.friendsContainer}>
+   
+    //             {friends.map(({avatar, name, isOnline, id}) => (
+       
+    //       <li key={id}className={styles.eachFriend}>
+    //             <span className={isOnline ? styles.statisticStatusGreen : styles.statisticStatusRed}></span>
+    //         <span>{name}</span>
+    //         <img className={styles.friendsImg} src={avatar}></img>
+    //       </li>
+    //     ))}
+      
+    // </ul>
+      
+       
+ 
+  
   );
 };
+
+
+
 
 
 
